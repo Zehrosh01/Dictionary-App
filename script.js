3b7aa0f036fa3e6fc2e6efc4d57c03d9768eaf03
Zehrosh01 <104352088+Zehrosh01@users.noreply.github.com>
2024-10-09 01:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-let form = document.querySelector(".my-form");
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault() // This prevents the window from reloading
+const apiKey = 'ac99ed9900msh31094520177d4b2p141c4fjsnaf40410d1c68';
 
-  let formdata = new FormData(this);
-  let input = formdata.get("searchTxt");
-  apiCall(input);
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.getElementById('search-input');
+const wordHeading = document.getElementById('word-heading');
+const definition = document.getElementById('definition');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const word = searchInput.value;
+  if (word) {
+    fetchDefinition(word);
+  }
 });
 
-
-
-
-
-async function apiCall(text) {
-  const url = 'https://urban-dictionary7.p.rapidapi.com/v0/define?term=' + text;
+async function fetchDefinition(word) {
+  const url = `https://urban-dictionary7.p.rapidapi.com/v0/define?term=${word}`;
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ac99ed9900msh31094520177d4b2p141c4fjsnaf40410d1c68',
+      'X-RapidAPI-Key': apiKey,
       'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com'
     }
   };
 
   try {
     const response = await fetch(url, options);
-    const result = await response.text();
-    const jsonResult = JSON.parse(result);
-    const recordsList = jsonResult['list'];
-    var index = 0;
+    const data = await response.json();
+    const definitions = data.list;
 
-
-    recordsList.forEach(element => {
-      index++;
-      document.getElementById('myData').innerHTML += "<p>";
-      document.getElementById('myData').innerHTML += "<b>" + index + ".</b> " + element["definition"] + "<br>";
-      document.getElementById('myData').innerHTML += "&nbsp;&nbsp;&nbsp;<a target='_blank' href=" + element["permalink"] + ">" + element["permalink"] + "</a><br><br>";
-      document.getElementById('myData').innerHTML += "</p>";
-    });
-
-    // alert(jsonResult['list'][0]["definition"]);
+    if (definitions.length > 0) {
+      wordHeading.textContent = definitions[0].word;
+      definition.textContent = definitions[0].definition;
+    } else {
+      wordHeading.textContent = 'Word not found';
+      definition.textContent = '';
+    }
   } catch (error) {
     console.error(error);
-    alert(error);
+    wordHeading.textContent = 'Error fetching definition';
+    definition.textContent = '';
   }
 }
